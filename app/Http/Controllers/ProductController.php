bcc29bc055303379ec4aafc2b1f24591be38d138
ziaul75 <ziaulchowdhury75@gmail.com>
2023-12-16 01:51:04 +0600

    Storekeeper-

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,8 +3,36 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('products.form');
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required|string',
+            'quantity' => 'required|integer',
+            'price' => 'required|numeric',
+        ]);
+
+        Product::create($data);
+
+        return redirect('/')->with('success', 'Product added successfully');
+    }
+
+    public function dashboard()
+    {
+        return view('dashboard');
+    }
+
+    public function transactions()
+    {
+        $transactions = Product::all(); 
+        return view('transactions', compact('transactions'));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,11 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+// routes/web.php
+
 use App\Http\Controllers\ProductController;
 
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('/', [ProductController::class, 'index']);
-Route::get('/products', [ProductController::class, 'showAll']);
+Route::post('/products', [ProductController::class, 'store']);
 Route::get('/dashboard', [ProductController::class, 'dashboard']);
-
-
+Route::get('/transactions', [ProductController::class, 'transactions']);
 
